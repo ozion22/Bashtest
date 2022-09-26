e0cf6d08ea5af08e13ee6833c787ca5499602f0c
ozion22 <94684892+ozion22@users.noreply.github.com>
2022-09-26 12:48:20 +0200

    yes

--- doesnothing.sh
+++ doesnothing.sh
@@ -5,7 +5,7 @@ echo "Well, I dont care!"
 while true; do
     echo "File created!"
     touch donothing$num.txt
-    let "num=num+1"
     echo "Does nothing" >> donothing$num.txt
+    let "num=num+1"
     sleep 1
 done

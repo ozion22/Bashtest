0d6c90e5b965bc54f1055da9948bcdc5776c6781
ozion22 <94684892+ozion22@users.noreply.github.com>
2022-09-26 15:41:01 +0200

    Yes.

--- doesnothing.sh
+++ doesnothing.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 echo "ready to do nothing?!"
-read yn
 echo "Well, I dont care!"
 while true; do
     echo "File created!"
-    #touch donothing$num.txt
-    #echo "Does nothing" >> donothing$num.txt
-    let "num=num+1"
-    sleep 1
+    touch donothing$num.txt
+    echo "Does nothing" >> donothing$num.txt
+    ((num=num+1))
+    sleep 0.01
 done

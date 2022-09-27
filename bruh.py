da87c9976f288263ede674f5eff997feefa6e491
ozion22 <94684892+ozion22@users.noreply.github.com>
2022-09-27 18:52:28 +0200

    a

--- bruh.py
+++ bruh.py
@@ -4,4 +4,4 @@ for i in range(100):
     print(f"Iteration: {i+1}")
     num=random.randint(1,100)
     print(f"Random number: {num}")
-    time.sleep(0.25)
+    time.sleep(0.01)
--- doesnothing.sh
+++ doesnothing.sh
@@ -3,8 +3,8 @@ echo "ready to do nothing?!"
 echo "Well, I dont care!"
 while true; do
     echo "File created!"
-    touch donothing$num.txt
-    echo "Does nothing" >> donothing$num.txt
+    touch donothing"$num".txt
+    echo "Does nothing" >> donothing"$num".txt
     ((num=num+1))
-    sleep 0.01
+    sleep 2s
 done

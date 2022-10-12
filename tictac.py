fef3145df64e5eeca15425634f7e85eb84a74668
ozion22 <94684892+ozion22@users.noreply.github.com>
2022-10-12 23:04:35 +0200

    Finishing touches :|>

--- tictac.py
+++ tictac.py
@@ -90,9 +90,9 @@ def game():
     def printboard(won):
         global x_array, y_array, z_array
         print(x_array[0], x_array[1], x_array[2], sep=" | ")
-        print("---+---+---")
+        print("--+---+--")
         print(y_array[0], y_array[1], y_array[2], sep=" | ")
-        print("---+---+---")
+        print("--+---+--")
         print(z_array[0], z_array[1], z_array[2], sep=" | ")
         if won != 1:
             makemove()
@@ -108,18 +108,18 @@ def game():
             Player = "X"
         print("Player: " + Player)
         # Takes move input
-        xm = input("Which column?\n")
-        ym = input("Which row?\n")
+        xm = int(input("Which column?\n"))
+        ym = int(input("Which row?\n"))
         # Passes the move with checkmove(), which then assigns values, unless the square is already filled
         if 1 > ym or xm > 3:  # Checks for invalid coordinates, prompts a new move
             print("Please enter valid coordinates (1-3)")
             makemove()
-        elif int(ym) == 1:
-            checkmove(x_array[int(xm)-1], x_array, int(xm))
-        elif int(ym) == 2:
-            checkmove(y_array[int(xm)-1], y_array, int(xm))
-        elif int(ym) == 3:
-            checkmove(z_array[int(xm)-1], z_array, int(xm))
+        elif ym == 1:
+            checkmove(x_array[xm-1], x_array, xm)
+        elif ym == 2:
+            checkmove(y_array[xm-1], y_array, xm)
+        elif ym == 3:
+            checkmove(z_array[xm-1], z_array, xm)
         # Passes the player value into is_won, if it returns true, prints the final board and announces the winner
         else:
             print("You didn't enter an interger, did you?")
